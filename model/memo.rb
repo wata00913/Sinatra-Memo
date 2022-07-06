4add959f4cf6c5a8ce8bde205dd1eda96da57346
wata00913 <175d8639@gmail.com>
2022-07-06 14:37:29 +0900

    memoクラスを作成

---
+++ model/memo.rb
@@ -0,0 +1,12 @@
+require 'securerandom'
+
+class Memo
+  attr_reader :id
+  attr_accessor :title, :content
+
+  def initialize(id = nil, title, content)
+    @id = id || SecureRandom.uuid
+    @title = title
+    @content = content
+  end
+end
