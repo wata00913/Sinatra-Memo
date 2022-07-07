2533fbf04e7057388ba51e5bc29fc13c2b0ea7c9
wata00913 <175d8639@gmail.com>
2022-07-08 08:23:02 +0900

    新規画面を作成

--- app.rb
+++ app.rb
@@ -5,12 +5,20 @@ get '/' do
   redirect '/memos'
 end
 
-get '/memos' do
-  @title = 'メモアプリ一覧'
+before do
   memo_struct = Struct.new('Memo', 'id', 'title', 'content')
   memo = memo_struct.new('1', 'メモ', 'メモです')
   @memos = [memo_struct.new('1', 'メモ', 'メモです'),
             memo_struct.new('2', 'memo', 'this is memo')
   ]
+end
+
+get '/memos' do
+  @title = 'メモアプリ一覧'
   erb :memos
+end
+
+get '/new' do
+  @title = '新規作成'
+  erb :new_memo
 end
