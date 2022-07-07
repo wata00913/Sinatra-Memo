6aba121f385f160c63de76a30bc780b3db3ee07d
wata00913 <175d8639@gmail.com>
2022-07-08 08:31:18 +0900

    /memosのpostメソッドを追加

--- app.rb
+++ app.rb
@@ -9,13 +9,16 @@ before do
   memo_struct = Struct.new('Memo', 'id', 'title', 'content')
   memo = memo_struct.new('1', 'メモ', 'メモです')
   @memos = [memo_struct.new('1', 'メモ', 'メモです'),
-            memo_struct.new('2', 'memo', 'this is memo')
-  ]
+            memo_struct.new('2', 'memo', 'this is memo')]
 end
 
 get '/memos' do
   @title = 'メモアプリ一覧'
   erb :memos
+end
+
+post '/memos' do
+  redirect 'memos'
 end
 
 get '/memos/new' do
