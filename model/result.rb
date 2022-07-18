67c46b35c9d84b20c76d560010c8b27a53dab5f5
wata00913 <175d8639@gmail.com>
2022-07-18 22:30:46 +0900

    リファクタリング用にインスタンス変数とラッパーメソッドを追加

--- model/result.rb
+++ model/result.rb
@@ -1,13 +1,13 @@
 class Result
-  attr_reader :data, :message
+  attr_reader :data, :msg
 
   class << self
-    def success(data: nil, message: nil)
-      new(success: true, data: data, message: message)
+    def success(data: nil, msg: nil, result: nil)
+      new(success: true, data: data, msg: msg, result: result)
     end
 
-    def fail(data: nil, message: nil)
-      new(success: false, data: data, message: message)
+    def fail(data: nil, msg: nil, result: nil)
+      new(success: false, data: data, msg: msg, result: result)
     end
   end
 
@@ -15,11 +15,16 @@ class Result
     @success
   end
 
+  def [](attr)
+    instance_variable_get "@#{attr}"
+  end
+
   private
 
-  def initialize(success:, data:, message:)
+  def initialize(success:, data:, msg:, result:)
     @success = success
     @data = data
-    @message = message
+    @msg = msg
+    @result = result
   end
 end
