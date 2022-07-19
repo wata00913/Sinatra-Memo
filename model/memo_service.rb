7fe14f64eda4083794bd1788064459617428504c
wata00913 <175d8639@gmail.com>
2022-07-19 11:51:57 +0900

    不要な引数の削除

--- model/memo_service.rb
+++ model/memo_service.rb
@@ -16,15 +16,15 @@ class MemoService
   def create(title, content)
     new_memo = Memo.new(nil, title, content)
     @repository.register(new_memo)
-    Result.success(result: 'success', msg: '登録に成功しました')
+    Result.success(msg: '登録に成功しました')
   end
 
   def find_by(id)
     memo = @repository.find_by(id)
     if memo
-      Result.success(result: 'success', msg: '', data: memo)
+      Result.success(msg: '', data: memo)
     else
-      Result.fail(result: 'fail', msg: '該当するメモは存在しません')
+      Result.fail(msg: '該当するメモは存在しません')
     end
   end
 
@@ -33,16 +33,16 @@ class MemoService
 
     begin
       @repository.update(memo)
-      Result.success(result: 'success', msg: '変更に成功しました')
+      Result.success(msg: '変更に成功しました')
     rescue StandardError => e
-      Result.fail(result: 'fail', msg: e.message)
+      Result.fail(msg: e.message)
     end
   end
 
   def delete(id)
     @repository.delete(id)
-    Result.success(result: 'success', msg: '削除に成功しました')
+    Result.success(msg: '削除に成功しました')
   rescue StandardError => e
-    Result.fail(result: 'fail', msg: e.message)
+    Result.fail(msg: e.message)
   end
 end
