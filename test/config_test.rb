e52e1c1d02dd0cfea6b8a200aef19f9dc1185f6e
wata00913 <175d8639@gmail.com>
2022-07-20 19:37:58 +0900

    設定項目が空の場合を対応(Configモジュールの修正とテストコードの追加) 設定ファイルが空だとJSONパーサーがパースエラーを起こすため。

--- test/config_test.rb
+++ test/config_test.rb
@@ -23,12 +23,23 @@ class ConfigTest < Minitest::Test
     end
   end
 
+  def create_test_empty_data_file(path)
+    File.open(path, 'w')
+  end
+
   def teardown
     Config.clear
     FileUtils.rm(@config_path) if File.exist?(@config_path)
   end
 
   def test_default
+    assert_equal @default_json_data_path, Config.json_data_path
+    assert_equal @save_type, Config.save_type
+  end
+
+  def test_setting_without_data
+    create_test_empty_data_file(@config_path)
+    Config.read(@config_path)
     assert_equal @default_json_data_path, Config.json_data_path
     assert_equal @save_type, Config.save_type
   end
