cd5b72c9db7044e76b40cdc3ef6f658e4e123aef
wata00913 <175d8639@gmail.com>
2022-07-20 14:53:05 +0900

    引数を移動(リファクタリング)するために、移動後に使用する引数を追加。 引数の移動をしてない場合にWarningを出力する処理も追加。

--- model/memo.rb
+++ model/memo.rb
@@ -6,9 +6,21 @@ class Memo
   attr_reader :id
   attr_accessor :title, :content
 
-  def initialize(id = nil, title, content)
-    @id = id || SecureRandom.uuid
+  def initialize(_id = nil, title, content, id: nil)
+    warn('Please use id argument of hash variable instead of _id argument', uplevel: 1) unless alternative_for_refactor?(_id, id)
+    alternative_id = alternative_for_refactor?(_id, id) ? id : _id
+    @id = alternative_id || SecureRandom.uuid
     @title = title
     @content = content
+  end
+
+  private
+
+  def alternative_for_refactor?(old, new)
+    # 下記組み合わせは、置き換え済みの場合
+    # (old, new) = (nil, nil)
+    # (old, new) = (string, string)
+    # (old, new) = (nil, string)
+    !(old && new.nil?)
   end
 end
